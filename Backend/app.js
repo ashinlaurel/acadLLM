9311409ddc990e6e475a0d65cf38b35b5c120495
Arvind Ranjit <arvindranjit242@gmail.com>
2024-09-28 22:02:52 +0530

    package import bugfixes

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { OpenAI } = require('openai');
+const path = require('path');
+const fs = require('fs');
 // const { default: mongoose } = require('mongoose');
 const mongoose = require('mongoose');
 
